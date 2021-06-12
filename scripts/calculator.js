3040195477245db6d750597189ea0c7f1cd509e3
Chrononaut61 <85802714+Chrononaut61@users.noreply.github.com>
2021-06-12 21:15:29 +0300

    first upload

---
+++ scripts/calculator.js
@@ -0,0 +1,5 @@
+let currentResult = 0;
+
+currentResult = currentResult + 10
+
+outputResult(currentResult, '')
---
+++ scripts/vendor.js
@@ -0,0 +1,13 @@
+const userInput = document.getElementById('input-number');
+const addBtn = document.getElementById('btn-add');
+const subtractBtn = document.getElementById('btn-subtrack');
+const multiplyBtn = document.getElementById('btn-multiply')
+const multiplyBtn = document.getElementById('btn-divide');
+
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
+
+function outputResult(result, text){
+    currentResultOutput.textContent = result;
+    currentCalculationOutput.textContent = text;
+}
