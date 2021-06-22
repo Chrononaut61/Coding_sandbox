bf81b743dec451a75cd205025f3308ceeef2da96
Chrononaut61 <85802714+Chrononaut61@users.noreply.github.com>
2021-06-22 14:20:05 +0300

    Calculator complete
    +Some experiments with alert and navbars
     Expect new additions.

--- Cosmonaut/scripts/vendor.js
+++ Cosmonaut/scripts/vendor.js
@@ -4,7 +4,7 @@ const subtractBtn = document.getElementById('btn-subtrack');
 const multiplyBtn = document.getElementById('btn-multiply')
 const multiplyBtn = document.getElementById('btn-divide');
 
-const currentResultOutput = document.getElementById('current-result');
+const currentResultOutput = document.getElementById('screen');
 const currentCalculationOutput = document.getElementById('current-calculation');
 
 function outputResult(result, text){
